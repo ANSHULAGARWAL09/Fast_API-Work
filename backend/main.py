991d014fe38eaba90f3d035aa22f6b39f136827f
Agarwal <anshagarwal@deloitte.com>
2021-11-05 16:43:59 +0530

    Add remaining file for Connect to Database

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title=settings.Project_Title, version=settings.Project_Version)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app=FastAPI(title=settings.Project_Title, version=settings.Project_Version)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 @app.get("/")
 def hello_api():
